331637331ba8c8e23c9da5b36340510458811099
Volodymyr Sut <peace_everyone@protonmail.com>
2024-03-24 19:13:01 +0100

    make randomizer inclusive

--- homework4.js
+++ homework4.js
@@ -15,7 +15,7 @@ console.log(customIsNaN(null) === isNaN(null));
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
 
 function checkProbabilityTheory(count) {
